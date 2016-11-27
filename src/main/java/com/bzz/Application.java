5f9226aae48cf868202076c642cf2218f8765c65
baozhizhi <baozhizhi850709@gmail.com>
2016-11-27 17:20:09 +0800

    初始化项目

---
+++ src/main/java/com/bzz/Application.java
@@ -0,0 +1,24 @@
+package main.java.com.bzz;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
+@Controller
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World! \t 你好大熊";
+    }
+
+}
